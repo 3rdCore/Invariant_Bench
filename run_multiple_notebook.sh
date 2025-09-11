0b04d575535bbdbc286940c8969c0c3ead59580c
3rdCore <tom.marty@mila.quebec>
2025-09-11 18:41:36 -0400

    made a LOOOOOT of changes in the training loop

--- run_multiple_notebook.sh
+++ run_multiple_notebook.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
+'''
+pairs=(
+  "0.5 0.05"
+  "0.5 0.1"
+  "0.5 0.2"
+  "0.5 0.5"
+)'''
+pairs=(
+  "0.5 0.05"
+)
 
-# Change to the project directory
-cd /home/mila/t/tom.marty/invariant_bench/SubpopBench/
+result_timestamp=$(date +%Y%m%d-%H%M%S)
+# Print job submission information
+echo "Results will be saved under results/${result_timestamp}/"
 
-for attr_prob in 0.01 0.1 0.5; do
-  for spur_prob in 0.01 0.1 0.5; do
+for seed in {0..4}
+do
+  for pair in "${pairs[@]}"; do
+    attr_prob=$(echo $pair | awk '{print $1}')
+    spur_prob=$(echo $pair | awk '{print $2}')
+    folder=${result_timestamp}/${attr_prob}_${spur_prob}/${seed}
+    
     export CMNIST_ATTR_PROB=$attr_prob
     export CMNIST_SPUR_PROB=$spur_prob
-    export RESULTS_TIMESTAMP=$(date +%Y%m%d-%H%M%S)_${attr_prob}_${spur_prob}
+    export SEED=$seed
+    export RESULT_FOLDER=$folder
 
-    # Create a temporary sbatch script, launch it on the cluster, then delete it
-    job_script="run_pcl_job_${RESULTS_TIMESTAMP}.sh"
+    # Create a temporary sbatch script with a unique name
+    job_script="run_pcl_job_${attr_prob}_${spur_prob}_${seed}.sh"
     cat <<EOF > $job_script
 #!/bin/bash
-#SBATCH --job-name=pcl_${attr_prob}_${spur_prob}
-#SBATCH --output=subpopbench/results/${RESULTS_TIMESTAMP}/pcl_${attr_prob}_${spur_prob}-%j.out
-#SBATCH --error=subpopbench/results/${RESULTS_TIMESTAMP}/pcl_${attr_prob}_${spur_prob}-%j.err
-#SBATCH --time=24:00:00
+#SBATCH --job-name=pcl_${attr_prob}_${spur_prob}_${seed}
+#SBATCH --output=subpopbench/results/${folder}/pcl-%j.out
+#SBATCH --error=subpopbench/results/${folder}/pcl-%j.err
+#SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
@@ -27,16 +44,16 @@ cd /home/mila/t/tom.marty/invariant_bench/SubpopBench/subpopbench
 source ../../XRM/.venv/bin/activate
 export CMNIST_ATTR_PROB=$attr_prob
 export CMNIST_SPUR_PROB=$spur_prob
-export RESULTS_TIMESTAMP=$RESULTS_TIMESTAMP
-mkdir -p results/${RESULTS_TIMESTAMP}
-jupyter nbconvert --to notebook --execute pcl.ipynb --output results/${RESULTS_TIMESTAMP}/executed_notebook_attr${attr_prob}_spur${spur_prob}.ipynb
+export SEED=$seed
+export RESULT_FOLDER=$folder
+mkdir -p results/${RESULT_FOLDER}
+jupyter nbconvert --to notebook --execute pcl.ipynb --output results/${RESULT_FOLDER}/executed_notebook.ipynb
 EOF
 
     # Submit the job
     sbatch $job_script
     rm -f $job_script
   done
-
 done
 
 echo "Notebook jobs submitted!"
