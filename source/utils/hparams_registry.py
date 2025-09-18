ee922c79ece1f2d09d0b733b22299cc687374c20
3rdCore <tom.marty@mila.quebec>
2025-09-18 11:06:12 -0400

    added hparams for VAE

--- source/utils/hparams_registry.py
+++ source/utils/hparams_registry.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from source.utils import misc
 
 
@@ -98,6 +99,11 @@ def _hparams(algorithm, dataset, random_seed):
     elif algorithm == "DFR":
         _hparam("stage1_model", "model.pkl", lambda r: "model.pkl")
         _hparam("dfr_reg", 0.1, lambda r: 10 ** r.uniform(-2, 0.5))
+
+    elif algorithm == "VAE":
+        _hparam("vae_latent_dim", 32, lambda r: int(r.choice([16, 32, 64])))
+        _hparam("vae_lr", 1e-3, lambda r: 10 ** r.uniform(-4, -2))
+        _hparam("vae_beta", 1, lambda r: r.uniform(0.5, 2.0))
 
     # Dataset-and-algorithm-specific hparam definitions
     # Each block of code below corresponds to exactly one hparam. Avoid nested conditionals
